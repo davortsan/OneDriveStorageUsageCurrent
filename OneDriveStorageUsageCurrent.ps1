894dd0a2ddb187e3cefb00002da1a32dbc09d2f4
David <2305432+davortsan@users.noreply.github.com>
2022-02-10 22:30:27 +0100

    Add files via upload

---
+++ OneDriveStorageUsageCurrent.ps1
@@ -0,0 +1,7 @@
+﻿$AdminSiteURL = "https://<tenant_name>-admin.sharepoint.com" #Example https://wafsec-admin.sharepoint.com
+
+$cred = Get-Credential
+
+Connect-SPOService -Url $AdminSiteURL -Credential $cred
+
+Get-SPOSite -IncludePersonalSite $true -Limit all -Filter "Url -like '-my.sharepoint.com/personal/'" | Select Owner, StorageUsageCurrent, StorageQuota | Export-Csv -Path "C:\temp\OneDriveUsage.csv" -Delimiter ';'
